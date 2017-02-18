b3da23c5bd30f21231a5375276dfd53122aa0eb8
woke <woke@172.16.39.175>
2017-02-18 12:12:01 +0800

    construct application

--- source/class/discuz/discuz_application.php
+++ source/class/discuz/discuz_application.php
@@ -11,4 +11,11 @@ class discuz_application extends discuz_base{
         }
         return $object;
     }
+    
+    public function __construct() {
+        $this->_init_env();
+        $this->_init_config();
+        $this->_init_input();
+        $this->_init_output();
+    }
 }
