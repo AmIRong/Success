deb0bb38afff3d509b98cfd6c93bcee5a1428d55
woke <woke@172.16.39.175>
2017-02-18 12:28:52 +0800

    Fatal error: Call to undefined function return_bytes() in /usr/local/zend/apache2/htdocs/Success/source/class/discuz/discuz_application.php on line 43

--- source/function/function_core.php
+++ source/function/function_core.php
@@ -1,1 +1,18 @@
 <?php
+if(!defined('IN_DISCUZ')) {
+    exit('Access Denied');
+}
+
+define('DISCUZ_CORE_FUNCTION', true);
+
+function return_bytes($val) {
+    $val = trim($val);
+    $last = strtolower($val{strlen($val)-1});
+    switch($last) {
+        case 'g': $val *= 1024;
+        case 'm': $val *= 1024;
+        case 'k': $val *= 1024;
+    }
+    return $val;
+}
+
