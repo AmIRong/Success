8ac4ca35e1f9ebbc2cf880bcedc7dcbfd3d7da5e
woke <woke@172.16.39.175>
2017-02-18 13:59:38 +0800

    The requested URL /Success/source/class/install/index.php was not found on this server.

---
+++ source/class/install/index.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+@set_time_limit(1000);
+@set_magic_quotes_runtime(0);
+
+define('IN_DISCUZ', TRUE);
+define('IN_COMSENZ', TRUE);
+define('ROOT_PATH', dirname(__FILE__).'/../');
