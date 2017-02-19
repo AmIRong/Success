9457b431252bc66c233ae3c40ddcabeda8aacfc6
woke <woke@172.16.39.175>
2017-02-19 12:59:09 +0800

    Fatal error: Call to undefined function env_check() in /usr/local/zend/apache2/htdocs/Success/source/class/install/index.php on line 65

--- source/class/install/include/install_function.php
+++ source/class/install/include/install_function.php
@@ -118,3 +118,29 @@ EOT;
     $quit && exit();
 }
 
+function env_check(&$env_items) {
+    foreach($env_items as $key => $item) {
+        if($key == 'php') {
+            $env_items[$key]['current'] = PHP_VERSION;
+        } elseif($key == 'attachmentupload') {
+            $env_items[$key]['current'] = @ini_get('file_uploads') ? ini_get('upload_max_filesize') : 'unknow';
+        } elseif($key == 'gdversion') {
+            $tmp = function_exists('gd_info') ? gd_info() : array();
+            $env_items[$key]['current'] = empty($tmp['GD Version']) ? 'noext' : $tmp['GD Version'];
+            unset($tmp);
+        } elseif($key == 'diskspace') {
+            if(function_exists('disk_free_space')) {
+                $env_items[$key]['current'] = floor(disk_free_space(ROOT_PATH) / (1024*1024)).'M';
+            } else {
+                $env_items[$key]['current'] = 'unknow';
+            }
+        } elseif(isset($item['c'])) {
+            $env_items[$key]['current'] = constant($item['c']);
+        }
+
+        $env_items[$key]['status'] = 1;
+        if($item['r'] != 'notset' && strcmp($env_items[$key]['current'], $item['r']) < 0) {
+            $env_items[$key]['status'] = 0;
+        }
+    }
+}
