9371945da9bfed5f41964b7c73a32d0a346cc6fc
woke <woke@172.16.39.175>
2017-02-19 15:23:50 +0800

    dsetcookie

--- source/function/function_core.php
+++ source/function/function_core.php
@@ -80,3 +80,33 @@ function random($length, $numeric = 0) {
     return $hash;
 }
 
+function dsetcookie($var, $value = '', $life = 0, $prefix = 1, $httponly = false) {
+
+    global $_G;
+
+    $config = $_G['config']['cookie'];
+
+    $_G['cookie'][$var] = $value;
+    $var = ($prefix ? $config['cookiepre'] : '').$var;
+    $_COOKIE[$var] = $value;
+
+    if($value == '' || $life < 0) {
+        $value = '';
+        $life = -1;
+    }
+
+    if(defined('IN_MOBILE')) {
+        $httponly = false;
+    }
+
+    $life = $life > 0 ? getglobal('timestamp') + $life : ($life < 0 ? getglobal('timestamp') - 31536000 : 0);
+    $path = $httponly && PHP_VERSION < '5.2.0' ? $config['cookiepath'].'; HttpOnly' : $config['cookiepath'];
+
+    $secure = $_SERVER['SERVER_PORT'] == 443 ? 1 : 0;
+    if(PHP_VERSION < '5.2.0') {
+        setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure);
+    } else {
+        setcookie($var, $value, $life, $path, $config['cookiedomain'], $secure, $httponly);
+    }
+}
+
