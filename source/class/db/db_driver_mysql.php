765a5971baf798295c427dc574c00a8e49fef5cb
woke <woke@172.16.39.175>
2017-02-19 16:50:51 +0800

    db_driver_mysql.php

---
+++ source/class/db/db_driver_mysql.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: db_driver_mysql.php 33349 2013-05-30 09:00:26Z kamichen $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+class db_driver_mysql
+{
+    
+}
