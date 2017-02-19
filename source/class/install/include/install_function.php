d1b69e1af0d8c88ab57530a5e51c91a760cbc854
woke <woke@172.16.39.175>
2017-02-19 10:28:27 +0800

    Fatal error: Call to undefined function getgpc() in /usr/local/zend/apache2/htdocs/Success/source/class/install/include/install_lang.php on line 138

--- source/class/install/include/install_function.php
+++ source/class/install/include/install_function.php
@@ -3,3 +3,14 @@ if(!defined('IN_COMSENZ')) {
     exit('Access Denied');
 }
 
+function getgpc($k, $t='GP') {
+    $t = strtoupper($t);
+    switch($t) {
+        case 'GP' : isset($_POST[$k]) ? $var = &$_POST : $var = &$_GET; break;
+        case 'G': $var = &$_GET; break;
+        case 'P': $var = &$_POST; break;
+        case 'C': $var = &$_COOKIE; break;
+        case 'R': $var = &$_REQUEST; break;
+    }
+    return isset($var[$k]) ? $var[$k] : null;
+}
