bd5a64a8ddd0d065fda07da57266c10525e43f72
woke <woke@172.16.39.175>
2017-02-19 11:08:29 +0800

    Fatal error: Call to undefined function show_license() in /usr/local/zend/apache2/htdocs/Success/source/class/install/index.php on line 59

--- source/class/install/include/install_function.php
+++ source/class/install/include/install_function.php
@@ -33,3 +33,36 @@ function transfer_ucinfo(&$post) {
         $uchidden = '';
     }
 }
+
+function show_license() {
+    global $self, $uchidden, $step;
+    $next = $step + 1;
+    if(VIEW_OFF) {
+
+        show_msg('license_contents', lang('license'), 1);
+
+    } else {
+
+        show_header();
+
+        $license = str_replace('  ', '&nbsp; ', lang('license'));
+        $lang_agreement_yes = lang('agreement_yes');
+        $lang_agreement_no = lang('agreement_no');
+        echo <<<EOT
+</div>
+<div class="main" style="margin-top:-123px;">
+	<div class="licenseblock">$license</div>
+	<div class="btnbox marginbot">
+		<form method="get" autocomplete="off" action="index.php">
+		<input type="hidden" name="step" value="$next">
+		<input type="hidden" name="uchidden" value="$uchidden">
+		<input type="submit" name="submit" value="{$lang_agreement_yes}" style="padding: 2px">&nbsp;
+		<input type="button" name="exit" value="{$lang_agreement_no}" style="padding: 2px" onclick="javascript: window.close(); return false;">
+		</form>
+	</div>
+EOT;
+
+        show_footer();
+
+    }
+}
