a7a2e5cfd8c21a2b4f8fd302abb14570ca7d5afc
woke <woke@172.16.39.175>
2017-02-20 12:54:18 +0800

    discuz_database::object(

--- source/class/discuz/discuz_database.php
+++ source/class/discuz/discuz_database.php
@@ -3,10 +3,17 @@ if(!defined('IN_DISCUZ')) {
     exit('Access Denied');
 }
 class discuz_database {
+    
+    public static $db;
+    public static $driver;
     public static function init($driver, $config) {
         self::$driver = $driver;
         self::$db = new $driver;
         self::$db->set_config($config);
         self::$db->connect();
+    }
+    
+    public static function object() {
+        return self::$db;
     }
 }
