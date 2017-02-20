9799a0ff1aec1e49f11ebde930220ebf9b0443bf
woke <woke@172.16.39.175>
2017-02-20 14:31:26 +0800

    core:memory()

--- source/class/table/table_common_syscache.php
+++ source/class/table/table_common_syscache.php
@@ -6,4 +6,14 @@ if(!defined('IN_DISCUZ')) {
 
 class table_common_syscache extends discuz_table
 {
+    public function __construct() {
+    
+        $this->_table = 'common_syscache';
+        $this->_pk    = 'cname';
+        $this->_pre_cache_key = '';
+        $this->_isfilecache = getglobal('config/cache/type') == 'file';
+        $this->_allowmem = memory('check');
+    
+        parent::__construct();
+    }
 }
