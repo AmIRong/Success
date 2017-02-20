4ecdc6ab43c7fd6f05883a5e94730dbfed13a11d
woke <woke@172.16.39.175>
2017-02-20 14:48:53 +0800

    discuz_table::__construct

--- source/class/discuz/discuz_table.php
+++ source/class/discuz/discuz_table.php
@@ -7,4 +7,17 @@ if(!defined('IN_DISCUZ')) {
 
 class discuz_table extends discuz_base
 {
+
+    public function __construct($para = array()) {
+        if(!empty($para)) {
+            $this->_table = $para['table'];
+            $this->_pk = $para['pk'];
+        }
+        if(isset($this->_pre_cache_key) && (($ttl = getglobal('setting/memory/'.$this->_table)) !== null || ($ttl = $this->_cache_ttl) !== null) && memory('check')) {
+            $this->_cache_ttl = $ttl;
+            $this->_allowmem = true;
+        }
+        $this->_init_extend();
+        parent::__construct();
+    }
 }
