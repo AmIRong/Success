bb45a399e9bb6621607bb39dc1481c44abcf23c7
woke <woke@172.16.39.175>
2017-02-20 14:45:08 +0800

    discuz_memory构造函数

--- source/class/discuz/discuz_memory.php
+++ source/class/discuz/discuz_memory.php
@@ -6,5 +6,13 @@ if(!defined('IN_DISCUZ')) {
 
 class discuz_memory extends discuz_base
 {
+    public function __construct() {
+        $this->extension['redis'] = extension_loaded('redis');
+        $this->extension['memcache'] = extension_loaded('memcache');
+        $this->extension['apc'] = function_exists('apc_cache_info') && @apc_cache_info();
+        $this->extension['xcache'] = function_exists('xcache_get');
+        $this->extension['eaccelerator'] = function_exists('eaccelerator_get');
+        $this->extension['wincache'] = function_exists('wincache_ucache_meminfo') && wincache_ucache_meminfo();
+    }
     
 }
