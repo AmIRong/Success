5058060db7a144daa7d16d11c4654117c4a2b8c3
woke <woke@172.16.39.175>
2017-02-20 15:13:54 +0800

    discuz_database::query()

--- source/class/discuz/discuz_database.php
+++ source/class/discuz/discuz_database.php
@@ -24,4 +24,31 @@ class discuz_database {
     public static function errno() {
         return self::$db->errno();
     }
+    
+    public static function query($sql, $arg = array(), $silent = false, $unbuffered = false) {
+        if (!empty($arg)) {
+            if (is_array($arg)) {
+                $sql = self::format($sql, $arg);
+            } elseif ($arg === 'SILENT') {
+                $silent = true;
+    
+            } elseif ($arg === 'UNBUFFERED') {
+                $unbuffered = true;
+            }
+        }
+        self::checkquery($sql);
+    
+        $ret = self::$db->query($sql, $silent, $unbuffered);
+        if (!$unbuffered && $ret) {
+            $cmd = trim(strtoupper(substr($sql, 0, strpos($sql, ' '))));
+            if ($cmd === 'SELECT') {
+    
+            } elseif ($cmd === 'UPDATE' || $cmd === 'DELETE') {
+                $ret = self::$db->affected_rows();
+            } elseif ($cmd === 'INSERT') {
+                $ret = self::$db->insert_id();
+            }
+        }
+        return $ret;
+    }
 }
