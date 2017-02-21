ce698b93f7d4cc52eadabe17613dfa2bc05e9ad1
woke <woke@172.16.39.175>
2017-02-21 21:56:47 +0800

    if

--- source/module/forum/forum_index.php
+++ source/module/forum/forum_index.php
@@ -42,6 +42,31 @@ if($_G['setting']['indexhot']['status'] && $_G['cache']['heats']['expiration'] <
     updatecache('heats');
 }
 
+if($_G['uid'] && empty($_G['cookie']['nofavfid'])) {
+    $favfids = array();
+    $forum_favlist = C::t('home_favorite')->fetch_all_by_uid_idtype($_G['uid'], 'fid');
+    if(!$forum_favlist) {
+        dsetcookie('nofavfid', 1, 31536000);
+    }
+    foreach($forum_favlist as $key => $favorite) {
+        if(defined('IN_MOBILE')) {
+            $forum_favlist[$key]['title'] = strip_tags($favorite['title']);
+        }
+        $favfids[] = $favorite['id'];
+    }
+    if($favfids) {
+        $favforumlist = C::t('forum_forum')->fetch_all($favfids);
+        $favforumlist_fields = C::t('forum_forumfield')->fetch_all($favfids);
+        foreach($favforumlist as $id => $forum) {
+            if($favforumlist_fields[$forum['fid']]['fid']) {
+                $favforumlist[$id] = array_merge($forum, $favforumlist_fields[$forum['fid']]);
+            }
+            forum($favforumlist[$id]);
+        }
+
+    }
+}
+
 function get_index_online_details() {
     $showoldetails = getgpc('showoldetails');
     switch($showoldetails) {
