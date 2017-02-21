b085c03cf0aaaf828eb28dfb9af9cc71cea418f2
woke <woke@172.16.39.175>
2017-02-21 14:28:52 +0800

    discuz_process.php

--- source/class/discuz/discuz_process.php
+++ source/class/discuz/discuz_process.php
@@ -10,6 +10,63 @@ class discuz_process
         $ttl = $ttl < 1 ? 600 : intval($ttl);
         return discuz_process::_status('get', $process) || discuz_process::_find($process, $ttl);
     }
+    
+    private static function _status($action, $process) {
+        static $plist = array();
+        switch ($action) {
+            case 'set' : $plist[$process] = true; break;
+            case 'get' : return !empty($plist[$process]); break;
+            case 'rm' : $plist[$process] = null; break;
+            case 'clear' : $plist = array(); break;
+        }
+        return true;
+    }
+    
+    private static function _find($name, $ttl) {
+    
+        if(!discuz_process::_cmd('get', $name)) {
+            discuz_process::_cmd('set', $name, $ttl);
+            $ret = false;
+        } else {
+            $ret = true;
+        }
+        discuz_process::_status('set', $name);
+        return $ret;
+    }
+    
+    private static function _cmd($cmd, $name, $ttl = 0) {
+        static $allowmem;
+        if($allowmem === null) {
+            $mc = memory('check');
+            $allowmem = $mc == 'memcache' || $mc == 'redis';
+        }
+        if($allowmem) {
+            return discuz_process::_process_cmd_memory($cmd, $name, $ttl);
+        } else {
+            return discuz_process::_process_cmd_db($cmd, $name, $ttl);
+        }
+    }
+    
+    private static function _process_cmd_db($cmd, $name, $ttl = 0) {
+        $ret = '';
+        switch ($cmd) {
+            case 'set':
+                $ret = C::t('common_process')->insert(array('processid' => $name, 'expiry' => time() + $ttl), FALSE, true);
+                break;
+            case 'get':
+                $ret = C::t('common_process')->fetch($name);
+                if(empty($ret) || $ret['expiry'] < time()) {
+                    $ret = false;
+                } else {
+                    $ret = true;
+                }
+                break;
+            case 'rm':
+                $ret = C::t('common_process')->delete_process($name, time());
+                break;
+        }
+        return $ret;
+    }
 }
 
 ?>
