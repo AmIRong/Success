4210b9f42069bf71811da0cfbcfd0e46be3f494d
woke <woke@172.16.39.175>
2017-02-21 11:27:00 +0800

    table_common_session.php

--- source/class/table/table_common_session.php
+++ source/class/table/table_common_session.php
@@ -6,5 +6,11 @@ if(!defined('IN_DISCUZ')) {
 
 class table_common_session extends discuz_table
 {
+    public function __construct() {
     
+        $this->_table = 'common_session';
+        $this->_pk    = 'sid';
+    
+        parent::__construct();
+    }
 }
