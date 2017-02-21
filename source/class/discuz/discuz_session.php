bd28d5d9339a1b3d887221a6a74d737f343a5d62
woke <woke@172.16.39.175>
2017-02-21 11:17:37 +0800

    构造

--- source/class/discuz/discuz_session.php
+++ source/class/discuz/discuz_session.php
@@ -5,4 +5,15 @@ if(!defined('IN_DISCUZ')) {
 }
 
 class discuz_session {
+    
+    public function __construct($sid = '', $ip = '', $uid = 0) {
+        $this->old = array('sid' =>  $sid, 'ip' =>  $ip, 'uid' =>  $uid);
+        $this->var = $this->newguest;
+    
+        $this->table = C::t('common_session');
+    
+        if(!empty($ip)) {
+            $this->init($sid, $ip, $uid);
+        }
+    }
 }
